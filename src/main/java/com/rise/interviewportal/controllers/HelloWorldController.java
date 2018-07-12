2aaa1e97289d76532c39d6526c07210105b09ccf
Mohinesh Tayade <mohineshtayade123@gmail.com>
2018-07-12 06:01:19 +0530

    second commit

---
+++ src/main/java/com/rise/interviewportal/controllers/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.rise.interviewportal.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rise.interviewportal.models.Greeting;
+
+@RestController
+public class HelloWorldController {
+	private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/api/hello")
+    public Greeting hello(@RequestParam(value="name", defaultValue="World") String name) {
+    	return new Greeting(counter.incrementAndGet(),String.format(template, name));
+    }
+}
